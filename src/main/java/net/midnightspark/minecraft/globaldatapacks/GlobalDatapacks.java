855dcfa2c2b576fe2f5bcead5dbfd84bc22894ff
Elliot Thomas <elliot@midnightspark.net>
2021-12-30 16:17:03 +0000

    Initial commit.

---
+++ src/main/java/net/midnightspark/minecraft/globaldatapacks/GlobalDatapacks.java
@@ -0,0 +1,10 @@
+package net.midnightspark.minecraft.globaldatapacks;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+
+@Mod("globaldatapacks")
+public class GlobalDatapacks {
+    public GlobalDatapacks() {
+    }
+}
